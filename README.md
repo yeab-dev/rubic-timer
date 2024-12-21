3794fb7c7f252b52832dada1ba907cffa65036f2
yeabsera <yeab.dev@gmail.com>
2024-12-21 08:46:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rubic_timer
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/rubic_timer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rubic_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
